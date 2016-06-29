81ebbad4c80d680a65ccb39229940eda9bc84be6
lshannon <luke.shannon@gmail.com>
2016-06-29 14:03:13 -0400

    Checking In base application

---
+++ spring-security101-baseapp/src/main/java/com/lukeshannon/meetup/springsecurity/SpringSecurity101BaseappApplication.java
@@ -0,0 +1,12 @@
+package com.lukeshannon.meetup.springsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurity101BaseappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurity101BaseappApplication.class, args);
+	}
+}
