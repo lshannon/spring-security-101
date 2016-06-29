312e2c8bb9221e0d095bf30bab879c0dc00f4c45
lshannon <luke.shannon@gmail.com>
2016-06-29 15:17:12 -0400

    Security Configured Application

---
+++ spring-security101-step1/src/main/java/com/lukeshannon/meetup/springsecurity/SpringSecurity101Step1Application.java
@@ -0,0 +1,12 @@
+package com.lukeshannon.meetup.springsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurity101Step1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurity101Step1Application.class, args);
+	}
+}
---
+++ spring-security101-step1/src/main/java/com/lukeshannon/meetup/springsecurity/config/SecurityConfig.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package com.lukeshannon.meetup.springsecurity.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @author lshannon
+ *
+ */
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+}
