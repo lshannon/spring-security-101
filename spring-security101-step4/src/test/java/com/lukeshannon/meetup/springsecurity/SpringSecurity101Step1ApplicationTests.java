63ef625fc09805af75f27d6a9651601c2bf1d824
lshannon <luke.shannon@gmail.com>
2016-06-29 16:46:46 -0400

    Final Example with some refactoring

---
+++ spring-security101-step4/src/test/java/com/lukeshannon/meetup/springsecurity/SpringSecurity101Step1ApplicationTests.java
@@ -0,0 +1,18 @@
+package com.lukeshannon.meetup.springsecurity;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = SpringSecurity101Step4Application.class)
+@WebAppConfiguration
+public class SpringSecurity101Step1ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
